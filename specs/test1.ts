481a929b9474e9662e5d6f2ef9cd720c24f745b6
vikaschhabra18 <43637142+vikaschhabra18@users.noreply.github.com>
2020-07-15 23:34:25 +0530

    Update test1.ts
    Youtube link update

--- specs/test1.ts
+++ specs/test1.ts
@@ -3,9 +3,10 @@ import { browser, Config, protractor } from "protractor";
 console.log("hi")
 describe('Protractor Demo App', async function () {
     it('should have a title', async function () {
-        await browser.get('https://mail.google.com/');
-        console.log("Test 1")
+        await browser.get('https://www.youtube.com/watch?v=-8jUsj7T9m4');
+        console.log("Youtube")
         let title = await browser.getTitle()
-        expect(title).toContain("Gmail");
+        expect(title).toContain("Kolkata");
+        await browser.sleep(19000);
     });
 });

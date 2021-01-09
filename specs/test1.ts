db48e31a49d6ccbf5bb4bf97f35b54537f8ed871
vikaschhabra18 <43637142+vikaschhabra18@users.noreply.github.com>
2021-01-10 03:57:42 +0530

    count increases to 100

--- specs/test1.ts
+++ specs/test1.ts
@@ -8,7 +8,7 @@ describe('Protractor Demo App', async function () {
         let title = await browser.getTitle()
         expect(title).toContain("Manali");
         let url = 'https://www.youtube.com/watch?v=_WC3PXWpmZ4';
-        for (let i = 0; i < 10; i++) {
+        for (let i = 0; i < 100; i++) {
             await browser.executeScript("window.open(arguments[0], '_blank')", url);
             await browser.getAllWindowHandles().then(async (handles) => {
                 await browser.switchTo().window(handles[i]);    // pass the index, here assuming that

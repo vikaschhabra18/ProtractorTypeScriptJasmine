f6da7cae9eb26819eba52ad56fb32963ea9a1d7c
vikaschhabra18 <43637142+vikaschhabra18@users.noreply.github.com>
2021-01-10 04:06:26 +0530

    Update config.ts

--- config/config.ts
+++ config/config.ts
@@ -24,7 +24,7 @@ export const config: Config = {
     framework: 'jasmine',
     jasmineNodeOpts: {
         showColors: true,
-        defaultTimeoutInterval: 30000*1000*1000
+        defaultTimeoutInterval: 1000*60*60
     },
     specs: [
         "..//specs//test1.js"
